66e16e5c4a7b6dd591a1aa8eb3ff39860d8a5418
uh <xiaoyueclwy@163.com>
2026-05-18 21:53:28 +0800

    refactor: organize source code into operator_tools_package directory

--- operator_tools_package/__init__.py
+++ operator_tools_package/__init__.py
@@ -1,12 +1,10 @@
 """
 PyTorch Operator Dump & Precision Comparison Tool
 
-Single-file entry point for operator debugging tools.
-
 Usage:
-    # Context manager mode
-    from operator_tools import OperatorDumper, compare_operator_dumps
+    from operator_tools_package import OperatorDumper, compare_operator_dumps
     
+    # Context manager mode
     with OperatorDumper("/path/to/dumps") as dumper:
         model(input)
     
@@ -19,7 +17,8 @@ Usage:
     compare_operator_dumps("/path/to/dump1", "/path/to/dump2")
 """
 
-from operator_dumper import OperatorDumper
-from compare_dumps import compare_operator_dumps
+from .operator_dumper import OperatorDumper
+from .compare_dumps import compare_operator_dumps
 
 __all__ = ['OperatorDumper', 'compare_operator_dumps']
+__version__ = '0.1.0'
